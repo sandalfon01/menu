0dba9d02e93b8dad3a9197b2c7f0468599aabfc2
sandalfon01 <sandalfon01@gmail.com>
2023-04-08 18:20:58 -0500

    se corrige lo del menú pero falta el menu toggle (°ロ°)

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const menuTuggle=document.querySelector('.menuToggle')
-const navegation=document.querySelector('.navegation');
+const navigation=document.querySelector('.navigation');
 
 menuTuggle.onclik=function(){
-    navegation.classList.toggle('open')
+    navigation.classList.toggle('open')
 }
+const list=document.querySelectorAll('.list')
+function activarLink(){
+    list.forEach((item)=>
+    item.classList.remove('active'))
+    this.classList.add('active')
+    
+}
+list.forEach((item)=>
+item.addEventListener('click',activarLink)
+)
+console.log(activarLink);
